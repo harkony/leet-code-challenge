414b601d23e01d8c9c3702b7532c7b45952aa1ef
deli-mandu <deli.mandu@kakaocorp.com>
2020-02-16 17:13:55 +0900

    [Add] solutions of AddTwoNumber and TwoSum

--- src/harkony/AddTwoNumber.java
+++ src/harkony/AddTwoNumber.java
@@ -1,4 +1,71 @@
 package harkony;
 
 public class AddTwoNumber {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+
+        //init
+        int sum = getVal(l1) + getVal(l2) + carry;
+        carry = sum / 10;
+        int remain = sum % 10;
+        ListNode root = new ListNode(remain);
+        ListNode previous = root;
+        l1 = getNext(l1);
+        l2 = getNext(l2);
+
+        while (true) {
+            sum = getVal(l1) + getVal(l2) + carry;
+            //escape case
+            if (l1 == null && l2 == null && sum == 0)
+                break;
+            carry = sum / 10;
+            remain = sum % 10;
+            ListNode current = new ListNode(remain);
+            previous.next = current;
+            previous = current;
+            l1 = getNext(l1);
+            l2 = getNext(l2);
+        }
+        return root;
+    }
+
+    private static ListNode getNext(ListNode l) {
+        if (l != null)
+            return l.next;
+        return null;
+    }
+
+    private static int getVal(ListNode l) {
+        if (l == null)
+            return 0;
+        return l.val;
+
+    }
+
+    public static void main(String args[]) {
+        ListNode l1 = new ListNode(2);
+        ListNode l2 = new ListNode(0);
+        l1.next = l2;
+        l2.next = new ListNode(3);
+
+
+        ListNode l4 = new ListNode(5);
+        ListNode l5 = new ListNode(0);
+        l4.next = l5;
+        l5.next = new ListNode(4);
+
+
+        ListNode result = addTwoNumbers(l1, l4);
+        int a = result.val;
+    }
+
 }
--- src/harkony/TwoSum.java
+++ src/harkony/TwoSum.java
@@ -1,4 +1,26 @@
 package harkony;
 
 public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+        int[] solution = new int[2];
+        for (int i = 0; i < len - 1; i++) {
+            int base = nums[i];
+            for (int j = i + 1; j < len; j++) {
+                if (base + nums[j] == target) {
+                    solution[0] = i;
+                    solution[1] = j;
+                    return solution;
+                }
+            }
+        }
+        throw new RuntimeException();
+    }
+
+    public static void main(String[] args) {
+        int[] given = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(given, target);
+        System.out.println(result[0] + "." + result[1]);
+    }
 }
