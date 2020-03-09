550b5a907258199b4e86939bbcb0c80c655fc18c
deli-mandu <deli.mandu@kakaocorp.com>
2020-03-09 15:50:45 +0900

    [Refactor] ZigZagConversion solution

--- src/harkony/ZigZagConversion.java
+++ src/harkony/ZigZagConversion.java
@@ -2,52 +2,46 @@ package harkony;
 
 public class ZigZagConversion {
     public static void main(String[] args) {
-        String sample="PAYPALISHIRING";
-        int numRows=4;
-        String expected="PINALSIGYAHRPI";
-        String actual= convert(sample,numRows);
-        assert(actual.equals(expected));
+        String sample = "PAYPALISHIRING";
+        int numRows = 4;
+        String expected = "PINALSIGYAHRPI";
+        String actual = convert(sample, numRows);
+        assert (actual.equals(expected));
         System.out.println(actual);
 
     }
+
     public static String convert(String s, int numRows) {
-        int len=s.length();
-        int nPerIteration = 2*numRows-2;
-        if(nPerIteration==0)
+        int len = s.length();
+        int nPerIteration = 2 * numRows - 2;
+        if (nPerIteration == 0)
             return s;
-        int iteration= (len / (nPerIteration))+1;
-        int nColPerInteration = numRows-1;
-        char matrix[][]=new char[numRows][nColPerInteration*iteration];
+        StringBuilder[] sb = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++)
+            sb[i] = new StringBuilder();
 
 
-
-        for(int index=0;index<len;index++){
-            int carry= index/nPerIteration;
-            int remain = index%nPerIteration;
-            int baseCol=carry*nColPerInteration;
-            if(remain==0){
-                for(int low =0; low < numRows;low ++){
-                    matrix[low][baseCol]=s.charAt(index++);
-                    if(index==len)
-                        return convertMatrixToString(matrix);
+        for (int index = 0; index < len; index++) {
+            int remain = index % nPerIteration;
+            if (remain == 0) {
+                for (int low = 0; low < numRows; low++) {
+                    sb[low].append(s.charAt(index++));
+                    if (index == len)
+                        return convertMatrixToString(sb);
                 }
-                index-=1;
+                index -= 1;
                 continue;
             }
-            int offset = remain-numRows+1;
-            matrix[numRows-1-offset][baseCol+offset]= s.charAt(index);
+            int offset = remain - numRows + 1;
+            sb[numRows - 1 - offset].append(s.charAt(index));
         }
-        return convertMatrixToString(matrix);
+        return convertMatrixToString(sb);
     }
 
-    public static String convertMatrixToString(char matrix[][]){
-        StringBuilder sb=new StringBuilder();
-        for(int low=0;low<matrix.length;low++){
-            for(int col=0;col<matrix[0].length;col++){
-                char c= matrix[low][col];
-                if(c!='\0')
-                    sb.append(c);
-            }
+    public static String convertMatrixToString(StringBuilder[] sbArray) {
+        StringBuilder sb = new StringBuilder();
+        for (StringBuilder stringBuilder : sbArray) {
+            sb.append(stringBuilder);
         }
         return sb.toString();
     }
