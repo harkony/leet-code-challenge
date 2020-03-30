d8c44cf7809aff1d253883cb33bcc0980501caad
harkony <harkony@naver.com>
2020-03-30 19:12:25 +0900

    [Add] Week7 problem solving

---
+++ src/harkony/ContainerWithMostWater.java
@@ -0,0 +1,20 @@
+package harkony;
+
+public class ContainerWithMostWater {
+    public static int maxArea(int[] height) {
+        int length = height.length;
+        int max = 0;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = i + 1; j < length; j++) {
+                int vol = Math.min(height[i], height[j]) * (j - i);
+                max = vol > max ? vol : max;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int test[] = {1, 1};
+        System.out.println(maxArea(test));
+    }
+}
