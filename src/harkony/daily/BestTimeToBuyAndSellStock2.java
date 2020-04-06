64bf6c7f62b34d40ca038bf8f5ab31cb42a0ccc2
harkony <harkony@naver.com>
2020-04-06 17:25:43 +0900

    [Refactor] with best solution

--- src/harkony/daily/BestTimeToBuyAndSellStock2.java
+++ src/harkony/daily/BestTimeToBuyAndSellStock2.java
@@ -3,27 +3,10 @@ package harkony.daily;
 public class BestTimeToBuyAndSellStock2 {
     public static int maxProfit(int[] prices) {
         int sum = 0;
-        int buy = -1;
-        if(prices.length<2)
-            return 0;
-
         for (int i = 0; i < prices.length-1; i++) {
-            if(prices[i+1]>prices[i]){
-                if(buy==-1 || prices[i]<=buy) {
-                    buy = prices[i];
-                }
-            }
-            if(prices[i]>prices[i+1]){
-                if(buy!=-1) {
-                    sum += (prices[i] - buy);
-                    buy=-1;
-                }
-            }
-
+            if(prices[i+1]-prices[i]>0)
+                sum+=prices[i+1]-prices[i];
         }
-        if(prices[prices.length-1] >= prices[prices.length-2] && buy!=-1)
-            sum+=(prices[prices.length-1]-buy);
-
         return sum;
     }
 }
